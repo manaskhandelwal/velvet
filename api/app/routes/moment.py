95dd71457cb3dacca04421f68fd1abb997df0e2a
Manas Khandelwal <mkdpsj@gmail.com>
2023-11-19 07:05:12 +0530

    feat(api): add cherish moment method

--- api/app/routes/moment.py
+++ api/app/routes/moment.py
@@ -1,29 +1,52 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import Annotated
-
+from models.cherish_relation import CherishRelation
+from utils.errors import conflit_error
 from models.moment import Moment
 from utils.helpers import get_db, success_responce
-from schemas.moment import MomentCreateDto
+from schemas.moment import MomentCherishDto, MomentCreateDto
 from core.security import get_current_user
-
 
 
 router = APIRouter()
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+
 @router.post("/")
-async def create_moment(user_dep: user_dependency, db: db_dependency, dto: MomentCreateDto):
+async def create_moment(
+    user_dep: user_dependency, db: db_dependency, dto: MomentCreateDto
+):
     # FIX: Use NLP to check for negative content
-    
+
     moment = Moment(message=dto.message, user_id=user_dep.get("id"))
-    
+
     db.add(moment)
     db.commit()
     db.refresh(moment)
-    
+
     return success_responce()
 
 
+@router.post("/cherish")
+async def cherish_moment(
+    user_dep: user_dependency, db: db_dependency, dto: MomentCherishDto
+):
+    cherished = (
+        db.query(CherishRelation)
+        .filter(CherishRelation.user_id == user_dep.get("id"))
+        .filter(CherishRelation.moment_id == dto.moment_id)
+        .first()
+    )
 
+    if cherished:
+        raise conflit_error("cherish", "You have already cherished this moment")
+
+    cherish = CherishRelation(user_id=user_dep.get("id"), moment_id=dto.moment_id)
+
+    db.add(cherish)
+    db.commit()
+    db.refresh(cherish)
+
+    return success_responce()
--- api/app/schemas/moment.py
+++ api/app/schemas/moment.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 from typing import Optional
+
 
 class MomentCreateDto(BaseModel):
     message: Optional[str] = Field(min_length=1)
     photo: Optional[str] = Field(min_length=2)
+
+
+class MomentCherishDto(BaseModel):
+    moment_id: str = Field()
